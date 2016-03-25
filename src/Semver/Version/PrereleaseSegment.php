571d39396971fc6bb184bbfbf9c12dd0d36a354d
Niels Keurentjes <niels.keurentjes@omines.com>
2016-03-25 10:02:34 +0100

    Accidentally referenced new required file.

---
+++ src/Semver/Version/PrereleaseSegment.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Semver
+ * (c) Omines Internetbureau B.V. - www.omines.nl
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Omines\Semver\Version;
+
+/**
+ * PrereleaseSegment
+ *
+ * @author Niels Keurentjes <niels.keurentjes@omines.com>
+ */
+class PrereleaseSegment extends IdentifierSegment
+{
+}
